893a5e0fc051964f3b63a46b63443c29b901b534
Phillip Taylor <philip.taylor@sky.uk>
2026-05-14 12:20:37 +0100

    Fix hang in docker after switching to fuse fs.

--- run_mac.sh
+++ run_mac.sh
@@ -2,4 +2,4 @@
 
 cd /Users/pth758/git/phills_jupyterlab/
 
-docker run --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 -it --publish "9098:8888" -v="/Users/pth758/projects/jupyter_root:/workspace" -v="/Users/pth758/projects/jupyter_root/.local:/home/user/.local" phills-jupyterlab /usr/local/bin/jupyter-lab
+docker run --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 -it --publish "9098:8888" -v="/Users/pth758/projects/jupyter_root:/workspace" phills-jupyterlab /usr/local/bin/jupyter-lab --ServerApp.token=b342bh34b2jhb423 --LabApp.custom_css=True
